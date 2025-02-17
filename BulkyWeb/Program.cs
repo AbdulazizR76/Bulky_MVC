c3395fc9cb18d29cd3d08d92ad1c413c320768d6
Abdulaziz.dev <a.alkhaldi@k-devs.com>
2025-02-17 16:14:15 +0300

    reset database

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("Home")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("Work")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
