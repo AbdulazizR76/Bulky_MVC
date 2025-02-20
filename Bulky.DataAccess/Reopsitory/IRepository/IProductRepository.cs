836b032f3b19c099700926d86963768210997678
Abdulaziz.dev <a.alkhaldi@k-devs.com>
2025-02-20 12:49:42 +0300

    ProductRepository and unitOfWork is done

---
+++ Bulky.DataAccess/Reopsitory/IRepository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Reopsitory.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void update(Product obj);
+    }
+}
---
+++ Bulky.DataAccess/Reopsitory/IRepository/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Reopsitory.IRepository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
