cab7089bba2b30fbf5e0f0081e1bce30551b4771
azoz <az.dev.76@gmail.com>
2025-02-25 20:35:56 +0300

    Combine, Create, and Edit Pages

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             // using Projections to dynamically select the fields we want to display
             //ViewBag.CategoryList = CategoryList;
@@ -36,11 +36,22 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 }),
                 Product = new Product(),
             };
-            return View(productVM);
+            if(id == null || id ==00)
+            {
+                //Create
+                return View(productVM);
+            }
+            else
+            {
+                // update
+                productVM.Product = _unitOfWork.Product.Get(u=>u.Id==id);
+                return View(productVM);
+            }
+                
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             
             if (ModelState.IsValid)
@@ -60,37 +71,6 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             }
                 return View(productVM);
         }
-
-
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            Product productFromDb = _unitOfWork.Product.Get(o => o.Id == id);
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "product updated succfulliy ";
-                return RedirectToAction("Index", "Product");
-            }
-            return View();
-        }
-
 
         public IActionResult Delete(int? id)
         {
