1a852e0406fe0e9e21690c730ce62f4968d647e1
azoz <az.dev.76@gmail.com>
2025-04-30 23:37:30 +0300

    DBInitializer Implementation

--- Bulky.DataAccess/DbInitializer/DbInitializer.cs
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +13,65 @@ namespace BulkyBook.DataAccess.DbInitializer
 {
     internal class DbInitializer : IDbInitializer
     {
+        private readonly ApplicationDbContext _db;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DbInitializer(
+            ApplicationDbContext db,
+            RoleManager<IdentityRole> roleManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
         public void Initialize()
         {
-            throw new NotImplementedException();
+
+
+            //migrations if they are not applied
+
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }catch (Exception ex)
+            {
+                throw;
+            }
+
+            //create roles if they are not created 
+
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+
+                //if roles are not created, then we will create admin user as well 
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "az.dev.76@gmail.com",
+                    Email = "az.dev.76@gmail.com",
+                    Name = "Admin",
+                    StreetAddress = "123 Admin St",
+                    City = "Admin City",
+                    State = "IL",
+                    PostalCode = "12121",
+                    PhoneNumber = "6669990000"
+                }, "Test@123").GetAwaiter().GetResult();
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "az.dev.76@gmail.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+
+            return;
+        }
+
         }
     }
-}
+
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using BulkyBook.Utility;
 using Stripe;
+using BulkyBook.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -42,7 +43,7 @@ builder.Services.AddSession(options =>
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
-
+builder.Services.AddScoped<IDbInitializer, IDbInitializer>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
@@ -64,6 +65,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseSession();
+SeedDatabase();
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
@@ -75,3 +77,12 @@ app.MapControllerRoute(
 
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
