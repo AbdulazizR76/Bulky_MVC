207975a210e6c1613a603155f8e2c42eee53626f
azoz <az.dev.76@gmail.com>
2025-05-20 22:48:12 +0300

    Add Product Image Repository

---
+++ Bulky.DataAccess/Reopsitory/IRepository/IProductImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Reopsitory.IRepository
+{
+    public interface IProductImageRepository : IRepository<ProductImage>
+    {
+        void Update(ProductImage obj);
+
+    }
+}
--- Bulky.DataAccess/Reopsitory/UnitOfWork.cs
+++ Bulky.DataAccess/Reopsitory/UnitOfWork.cs
@@ -15,6 +15,7 @@ namespace BulkyBook.DataAccess.Reopsitory
         public IApplicationUserRepository ApplicationUser { get; private set; }
         public IOrderHeaderRepository OrderHeader { get; private set; }
         public IOrderDetailRepository OrderDetail { get; private set; }
+        public IProductImageRepository ProductImage { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
@@ -26,6 +27,7 @@ namespace BulkyBook.DataAccess.Reopsitory
             ApplicationUser = new ApplicationUserRepository(_db);
             OrderHeader = new OrderHeaderRepository(_db);
             OrderDetail = new OrderDetailRepository(_db);
+            ProductImage = new ProductImageRepository(_db);
 
         }
 
